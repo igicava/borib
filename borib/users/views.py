cd3171ac76abab8b6c74873662b14562f7f47090
Alexander Sudo <alexanderurchenco.ru@gmail.com>
2024-02-19 00:46:58 +0300

    modified:   borib/db.sqlite3 	new file:   borib/users/__pycache__/forms.cpython-310.pyc 	modified:   borib/users/__pycache__/views.cpython-310.pyc 	new file:   borib/users/forms.py 	modified:   borib/users/templates/users/login.html 	modified:   borib/users/templates/users/register.html 	modified:   borib/users/views.py

--- borib/users/views.py
+++ borib/users/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from users.models import User
+from users.forms import UserLoginForm
+from django.contrib import auth
+from django.urls import reverse
 
 def login(request):
-    return render(request, 'users/login.html')
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        context = {
+            'form': UserLoginForm()
+        }
+        return render(request, 'users/login.html', context)
 
 def register(request):
     return render(request, 'users/register.html')
