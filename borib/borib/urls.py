9128a16488457cf857761cd9be354937d59aed8c
Alexander Sudo <alexanderurchenco.ru@gmail.com>
2024-03-11 18:25:16 +0300

    deleted:    borib/borib/__pycache__/__init__.cpython-310.pyc 	deleted:    borib/borib/__pycache__/settings.cpython-310.pyc 	deleted:    borib/borib/__pycache__/urls.cpython-310.pyc 	deleted:    borib/borib/__pycache__/wsgi.cpython-310.pyc 	modified:   borib/borib/urls.py 	deleted:    borib/main/__pycache__/__init__.cpython-310.pyc 	deleted:    borib/main/__pycache__/admin.cpython-310.pyc 	deleted:    borib/main/__pycache__/apps.cpython-310.pyc 	deleted:    borib/main/__pycache__/models.cpython-310.pyc 	deleted:    borib/main/__pycache__/views.cpython-310.pyc 	new file:   borib/main/templates/error.html 	modified:   borib/main/views.py 	deleted:    borib/messenger/__pycache__/__init__.cpython-310.pyc 	deleted:    borib/messenger/__pycache__/admin.cpython-310.pyc 	deleted:    borib/messenger/__pycache__/apps.cpython-310.pyc 	deleted:    borib/messenger/__pycache__/models.cpython-310.pyc 	deleted:    borib/messenger/__pycache__/urls.cpython-310.pyc 	deleted:    borib/messenger/__pycache__/views.cpython-310.pyc 	deleted:    borib/users/__pycache__/__init__.cpython-310.pyc 	deleted:    borib/users/__pycache__/admin.cpython-310.pyc 	deleted:    borib/users/__pycache__/apps.cpython-310.pyc 	deleted:    borib/users/__pycache__/forms.cpython-310.pyc 	deleted:    borib/users/__pycache__/models.cpython-310.pyc 	deleted:    borib/users/__pycache__/urls.cpython-310.pyc 	deleted:    borib/users/__pycache__/views.cpython-310.pyc

--- borib/borib/urls.py
+++ borib/borib/urls.py
@@ -4,6 +4,10 @@ from django.conf.urls.static import static
 from django.conf import settings
 from main import views
 
+handler403 = 'main.views.tr_handler403'
+handler404 = 'main.views.tr_handler404'
+handler500 = 'main.views.tr_handler500'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name="index"),
--- borib/main/views.py
+++ borib/main/views.py
@@ -7,3 +7,32 @@ def index(request):
         "posts": News.objects.all().order_by('-date'),
     }
     return render(request, 'main/index.html', context)
+
+def tr_handler404(request, exception):
+    """
+    Обработка ошибки 404
+    """
+    return render(request=request, template_name='error.html', status=404, context={
+        'title': 'Страница не найдена: 404',
+        'error_message': 'К сожалению такая страница была не найдена, или перемещена', 
+    })
+
+
+def tr_handler500(request):
+    """
+    Обработка ошибки 500
+    """
+    return render(request=request, template_name='error.html', status=500, context={
+        'title': 'Ошибка сервера: 500',
+        'error_message': 'Внутренняя ошибка сайта, вернитесь на главную страницу, отчет об ошибке мы направим администрации сайта',
+    })
+
+
+def tr_handler403(request, exception):
+    """
+    Обработка ошибки 403
+    """
+    return render(request=request, template_name='error.html', status=403, context={
+        'title': 'Ошибка доступа: 403',
+        'error_message': 'Доступ к этой странице ограничен',
+    })
